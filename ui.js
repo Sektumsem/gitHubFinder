3a1b4d66ad95b1761ecb099072cf39e8579f3e1e
Sektumsem <50651549+Sektumsem@users.noreply.github.com>
2019-09-30 09:56:03 +0300

    Ui

---
+++ ui.js
@@ -0,0 +1,55 @@
+class UI{
+    constructor(){
+        this.profile= document.getElementById('profile');
+    }
+    showProfile(user){
+        this.profile.innerHTML = `
+            <div class="card card-body mb-3">
+                <div class ="row">
+                    <div class="col-md-3">
+                        <img class="img-fluid mb-2" src="${user.avatar_url}">
+                        <a class="btn btn-primary btn-block mb-4" href="${user.html_url}">View profile</a>
+                    </div>
+                    <div class="col-md-9">
+                        <span class="badge badge-primary">Public repos: ${user.public_repos}</span>
+                        <span class="badge badge-success">Followers: ${user.followers}</span>
+                        <span class="badge badge-info">Following: ${user.following}</span>
+                    </div>
+                    <br>
+                    <br>
+                    <ul class="list-group">
+                        <li class="list-group-item">Company: ${user.company}</li>
+                        <li class="list-group-item">Blog: ${user.blog}</li>
+                        <li class="list-group-item">Location: ${user.locaton}</li>
+                        <li class="list-group-item">Member since: ${user.created_at}</li>
+                    </ul>
+                </div>
+            </div>
+    
+            <h3 class="page heading">Latest repos</h3>
+            <div id="repos"></div>
+    
+        `;
+    }
+    showRepos(repos){
+        let output="";
+        repos.forEach(function(repo){
+            output +=`
+            <div class="card card-body mb-2">
+                <div class="row">
+                    <div class="col-md-6">
+                    <a href="${user.html_url}">${repo.name}</a>
+                    </div>
+                    <div class="col-md-6">
+                        <span class="badge badge-primary">Stars: ${user.stargazers_count}</span>
+                        <span class="badge badge-secondary">Watchers: ${user.watchers_count}</span>
+                        <span class="badge badge-success">Followers: ${user.forks_count}</span>
+                    </div>
+                </div>
+            </div>
+            `
+        });
+
+    }
+}
+
